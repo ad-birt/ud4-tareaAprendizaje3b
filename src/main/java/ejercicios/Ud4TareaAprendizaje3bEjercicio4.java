3dd07e6421c3571c3ba32f3c1ddfe852e0582c5f
bmadina <bmadina@birt.eus>
2023-11-13 11:58:13 +0100

    initial commit

---
+++ src/main/java/ejercicios/Ud4TareaAprendizaje3bEjercicio4.java
@@ -0,0 +1,70 @@
+package ejercicios;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import entidades.Student;
+
+public class Ud4TareaAprendizaje3bEjercicio4 {
+
+	/**
+	 * 4. Borra un Student
+	 */
+	public static void main(String[] args) {
+
+		// crea sessionFactory y session
+		StandardServiceRegistry standardRegistry = new StandardServiceRegistryBuilder()
+			    .configure( "hibernate.cfg.xml" )
+			    .build();
+
+		Metadata metadata = new MetadataSources( standardRegistry )
+			    .addAnnotatedClass( Student.class )
+			    .getMetadataBuilder()
+			    .build();
+
+		SessionFactory sessionFactory = metadata.getSessionFactoryBuilder()
+			    .build();    
+		
+		Session session = sessionFactory.openSession();
+		
+		try {			
+			
+			System.out.println("Borrando un objeto Student");
+			
+			int student_id = 4;
+			
+			Student tempStudent = session.get(Student.class, student_id);
+			
+			// comienza la transacción
+			session.beginTransaction();
+		
+			// borra el student
+			session.remove(tempStudent);
+			
+			// hace commit de la transaccion
+			session.getTransaction().commit();
+					
+			System.out.println("Hecho!");
+		}
+		catch ( Exception e ) {
+			// rollback ante alguna excepción
+			System.out.println("Realizando Rollback");
+			session.getTransaction().rollback();
+			e.printStackTrace();
+		}
+		finally {
+			session.close();
+			sessionFactory.close();
+		}
+	}
+	
+}
+
+
+
+
+
